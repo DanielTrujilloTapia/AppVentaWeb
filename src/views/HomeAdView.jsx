69a675532302ec5c22fb1612fc0571c1208e4d72
DanielTrujilloTapia <22300119@uttt.edu.mx>
2024-03-14 02:09:47 -0600

    TablaUsuarios_ControllerAndView

--- src/views/HomeAdView.jsx
+++ src/views/HomeAdView.jsx
@@ -1,51 +1,9 @@
-import { useEffect, useState } from 'react';
+import HomeAdController from '../controller/HomeAdController';
 
 function HomeAdView() {
-  const [users, setUsers] = useState([]);
-  const apiUrl = 'https://localhost:7199/api/Usu_Usuarios';
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div>
-      <h1>User Data Table</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Usuario</th>
-            <th>Contraseña</th>
-            <th>Estado</th>
-            <th>Tipo de cuenta</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id_usuario}>
-              <td>{user.id_usuario}</td>
-              <td>{user.nom_usuario}</td>
-              <td>{user.contrasena}</td>
-              <td>{user.idusucatestado}</td>
-              <td>{user.idusucattipousuario}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <HomeAdController></HomeAdController>
     </div>
   );
 }
